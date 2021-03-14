775bc342246f162041d977b79fd844be3987d9f8
Vladimir Novik <v.novik45@gmail.com>
2021-03-14 23:00:50 +0300

    Task 2

--- Python/DZ_1/time.py
+++ Python/DZ_1/time.py
@@ -5,17 +5,17 @@ if duration // 86400 != 0:
     days = int(duration / 3600 / 24)
     duration -= int(days * 86400)
     hour = duration // 3600
-    minutes = duration % 3600 / 60 
+    minutes = duration % 3600 / 60
     second = duration % 3600 % 60
     print(f'{int(days)} дн {int(hour)} час {int(minutes)} мин {int(second)} сек')
 elif duration // 3600 != 0:
-    hour = duration / 3600 
-    minutes = duration % 3600 / 60 
+    hour = duration / 3600
+    minutes = duration % 3600 / 60
     second = duration % 3600 % 60
     print(f'{int(hour)} час {int(minutes)} мин {int(second)} сек')
 elif duration // 60 != 0:
-    hour = duration / 3600 
-    minutes = duration % 3600 / 60 
+    hour = duration / 3600
+    minutes = duration % 3600 / 60
     second = duration % 3600 % 60
     print(f'{int(minutes)} мин {int(second)} сек')
 else:
