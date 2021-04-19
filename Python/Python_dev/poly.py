4f3f80b355224e3224c184bd459f5475993212ed
Vladimir Novik <v.novik45@gmail.com>
2021-04-20 01:26:47 +0300

    Complete DZ_2

---
+++ Python/Python_dev/poly.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+
+class ItemDiscount:
+    """
+    Create price product
+    """
+
+    def __init__(self, product, price):
+        self.__product = product
+        self.__price = price
+
+
+    def create_price_product(self):
+        price = {self.__product: self.__price}
+        return price
+
+
+class ItemDiscountReport(ItemDiscount):
+    def __init__(self, product, price, discount):
+        super().__init__(product, price)
+        self.discount = discount
+    
+
+    def __int__(self):
+        self.discount = discount
+
+
+    def __str__(self):
+        return str(self._ItemDiscount__price * (100 - self.discount)/100)
+
+
+
+class ItemDiscountReport_2(ItemDiscount):
+    def __init__(self, product, price, discount):
+        super().__init__(product, price)
+        self.discount = discount
+    
+
+    def __int__(self):
+        self.discount = discount
+
+
+    def __str__(self):
+        return str(self._ItemDiscount__price * (100 - self.discount)/100)
+
+
+
+car = ItemDiscount('mazda', 1000)
+discount_price = ItemDiscountReport('mazda', 1000, 10)
+print(discount_price)
+discount_price_2 = ItemDiscountReport_2('BMW', 1000, 30)
+print(discount_price_2)
